430de3b1934a93cd3956546af56650d581aa80e2
Tenidania <oluwateniola.dania2@mail.dcu.ie>
2023-11-06 11:45:04 +0000

    first quiz version

--- quiz.py
+++ quiz.py
@@ -1,3 +1,29 @@
+import random
+
+q_a = {
+    "Question 1": "What is the result of 2 + 3?",
+    "Answer 1": "5",
+    
+    "Question 2": "What is the result of 5 * 4?",
+    "Answer 2": "20",
+    
+    "Question 3": "What is the result of 10 / 2?",
+    "Answer 3": "5",
+    
+    "Question 4": "What is the square root of 25?",
+    "Answer 4": "5",
+    
+    "Question 5": "What is the result of 7 - 9?",
+    "Answer 5": "-2",
+    
+    # Add more questions and answers as needed.
+}
+
+# questions = list(q_a.keys())
+# random.shuffle(questions)
+
+
+
 print("Welcome to my quiz!")
 
 playing = input("Do you want to play? y/n: ").lower()
@@ -6,9 +32,48 @@ if playing != "y":
     quit()
 
 
-print("Ok, let's play!")
+print(f"Ok, let's play!\nYou must answer {len(q_a)//2} questions. If you get 3 wrong, you lose!")
 
-answer = input("What does CPU stand for? ").lower()
+score = 0
 
-if answer == "central processing unit":
-    print("correct")
+# q_a = {
+#     "Question 1": "What is the result of 2 + 3?",
+#     "Answer 1": "5",
+    
+#     "Question 2": "What is the result of 5 * 4?",
+#     "Answer 2": "20",
+    
+#     "Question 3": "What is the result of 10 / 2?",
+#     "Answer 3": "5",
+    
+#     "Question 4": "What is the square root of 25?",
+#     "Answer 4": "5",
+    
+#     "Question 5": "What is the result of 7 - 9?",
+#     "Answer 5": "-2",
+    
+#     # Add more questions and answers as needed.
+# }
+
+questions = [x for x in q_a.keys() if x[0] == "Q"]
+
+random.shuffle(questions)
+
+while len(questions) > 0:
+    quest = questions.pop()
+    answer = input(f"{q_a[quest]} ")
+
+
+    corr_ans = q_a[quest.replace("Question", "Answer")]
+
+    if answer == corr_ans or answer == "x":
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+    
+    
+if score > 0:
+    print(f"You answered {score} questions correctly")
+else:
+    print("You lost")
